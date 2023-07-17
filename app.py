c3b312d119d5963009446d44d4a79f46f4ef6ffa
beheshta <husna300@gmail.com>
2023-07-17 13:35:22 +0500

    my reposit5

---
+++ app.py
@@ -0,0 +1,17 @@
+if 8 > 9 :
+    print("true") 
+
+if 3 == 3 :
+    print("True")
+
+if 11 > 6 :
+    print("true")
+
+if 22 > 4 :
+    print("true") 
+
+if 3 > 2 :
+    print("three is greather than two ")
+    
+if 0 > 3 :
+    print("true")
